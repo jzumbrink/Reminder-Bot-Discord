31e7d51c9d2e92499344e7bb07938652b87d92ac
jzumbrink <j.zumbrink@gmx.de>
2021-09-29 16:25:07 +0200

    addition of datetime and timedelta

--- datetime_handling/datetime_handling.py
+++ datetime_handling/datetime_handling.py
@@ -1,3 +1,6 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
 time_periods = {
     'year': 'y',
     'month': 'm',
@@ -13,14 +16,12 @@ time_formats = {
 }
 
 def extract_time_shortcuts(text: str):
-    time_periods_found = {}
+    time_periods_found = {value: 0 for value in time_periods.values()}
     new_msg = []
     for word in text.split(' '):
         for time_period in time_periods.values():
             if len(word) > len(time_period):
                 if time_period == word[-len(time_period):] and word[:-len(time_period)].isdigit():
-                    if time_period not in time_periods_found.keys():
-                        time_periods_found[time_period] = 0
                     time_periods_found[time_period] += int(''.join(filter(lambda c: c.isdigit(), word)))
                     break
         else:
@@ -28,4 +29,19 @@ def extract_time_shortcuts(text: str):
     return ' '.join(new_msg), time_periods_found
 
 
-print(extract_time_shortcuts("1w dsanjdfj dfdkjnfdn34w 45min 3d"))
+def add_time_shortcuts_to_datetime(time_shortcuts: dict, given_datetime: datetime.datetime):
+    return given_datetime + relativedelta(
+        years=time_shortcuts[time_periods['year']],
+        months=time_shortcuts[time_periods['month']],
+        weeks=time_shortcuts[time_periods['week']],
+        days=time_shortcuts[time_periods['day']],
+        hours=time_shortcuts[time_periods['hour']],
+        minutes=time_shortcuts[time_periods['minute']],
+        seconds=time_shortcuts[time_periods['second']],
+    )
+
+
+def add_time_shortcuts_to_now(time_shortcuts: dict):
+    return add_time_shortcuts_to_datetime(time_shortcuts, datetime.datetime.now())
+
+print(add_time_shortcuts_to_now(extract_time_shortcuts("1w dsanjdfj dfdkjnfdn34w 45min 3d 23y 3w 3m")[1]))
